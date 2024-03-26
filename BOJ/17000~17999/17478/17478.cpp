6e4c9e2cad4ecc4dc4ced4717615d69bf4ae8eb4
heejinn <hjlee6824@naver.com>
2024-03-26 11:08:46 +0900

    코드 수정

--- BOJ/17000~17999/17478/17478.cpp
+++ BOJ/17000~17999/17478/17478.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N;
 string str = "";
 
-void func(int n) {
+void func(int cnt) {
     string indent = str;
 
     cout << indent << "\"재귀함수가 뭔가요?\"\n";
 
-    if (n == 0) {
+    if (cnt == N) {
         cout << indent << "\"재귀함수는 자기 자신을 호출하는 함수라네\"\n";
     }
     else {
@@ -17,7 +18,7 @@ void func(int n) {
         cout << indent << "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n";
 
         str += "____";
-        func(n - 1);
+        func(cnt + 1);
     }
 
     cout << indent << "라고 답변하였지.\n";
@@ -27,10 +28,9 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int N;
     cin >> N;
     cout << "어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n";
-    func(N);
+    func(0);
 
     return 0;
 }
