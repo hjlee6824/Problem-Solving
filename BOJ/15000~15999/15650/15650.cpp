14957635b2c8789aae9bb21d8decd83d82bc91a3
heejinn <hjlee6824@naver.com>
2024-03-28 17:16:54 +0900

    N과 M (2)

---
+++ BOJ/15000~15999/15650/15650.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[8];
+bool is_used[9];
+
+void func(int k) {
+    if (k == M) {
+        for (int i = 0; i < M; ++i)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; ++i) {
+        if (!is_used[i]) {
+            if (k >= 1 && i < arr[k - 1]) continue;
+            arr[k] = i;
+            is_used[i] = true;
+            func(k + 1);
+            is_used[i] = false;
+        }
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> N >> M;
+    func(0);
+    
+    return 0;
+}
