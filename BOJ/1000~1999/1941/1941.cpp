3e6c2a890660a1f4c02f61d4d14401bdb7320119
heejinn <hjlee6824@naver.com>
2024-04-04 13:04:54 +0900

    코드 수정 및 최적화

--- BOJ/1000~1999/1941/1941.cpp
+++ BOJ/1000~1999/1941/1941.cpp
@@ -4,18 +4,19 @@ using namespace std;
 #define X first
 #define Y second
 
-int ans = 0;
-char board[5][5];
+int ans;
+string seat[5];
 int mask[25];
-vector<pair<int, int>> coordinates;
-
-int arr[5][5];
+int board[5][5];
 bool visited[5][5];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
-void bfs(int x, int y) {
+// 영역이 전부 인접해있다면 BFS을 1회 수행했을 때 7개의 영역을 전부 방문했는지 확인
+bool bfs(vector<pair<int, int>>& v) {
     queue<pair<int, int>> q;
+    
+    int x = v.front().X, y = v.front().Y;
     visited[x][y] = true;
     q.push({x, y});
 
@@ -25,66 +26,53 @@ void bfs(int x, int y) {
         for (int dir = 0; dir < 4; ++dir) {
             int nx = cur.X + dx[dir];
             int ny = cur.Y + dy[dir];
+
             if (nx < 0 || nx >= 5 || ny < 0 || ny >= 5) continue;
-            if (arr[nx][ny] == 0 || visited[nx][ny]) continue;
+            if (board[nx][ny] == 0 || visited[nx][ny]) continue;
+
             visited[nx][ny] = true;
             q.push({nx, ny});
         }
     }
-}
 
-bool check1(vector<pair<int, int>>& v) { // S의 개수 검사
-    int S = 0;
-    for (auto cur : v)
-        if (board[cur.X][cur.Y] == 'S') ++S;
+    for (auto c : v)
+        if (!visited[c.X][c.Y])
+            return false;
 
-    return (S >= 4 ? true : false);
-}
-
-bool check2(vector<pair<int, int>>& v) { // 전부 인접한지 검사 (BFS)
-    int cnt = 0; // BFS 횟수
-
-    for (int i = 0; i < 5; ++i) {
-        fill(arr[i], arr[i] + 5, 0);
-        fill(visited[i], visited[i] + 5, false);
-    }
-
-    for (auto e : v) arr[e.X][e.Y] = 1;
-
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            if (arr[i][j] == 1 && !visited[i][j]) {
-                ++cnt;
-                bfs(i, j);
-            }
-        }
-    }
-
-    return (cnt == 1 ? true : false);
+    return true;
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-    for (int i = 0; i < 5; ++i) {
-        for (int j = 0; j < 5; ++j) {
-            cin >> board[i][j];
-            coordinates.push_back({i, j});
-        }
-    }
+    for (int i = 0; i < 5; ++i)
+        cin >> seat[i];
 
-    for (int i = 7; i < 25; ++i) mask[i] = 1; // 7개의 0과 나머지가 1로 된 수열
+    fill(mask + 7, mask + 25, 1);
 
     do {
-        vector<pair<int, int>> tmp;
-        for (int i = 0; i < 25; ++i)
-            if (mask[i] == 0) tmp.push_back(coordinates[i]);
+        int S = 0;
+        vector<pair<int, int>> v;
 
-        if (check1(tmp) && check2(tmp)) ++ans;
+        for (int i = 0; i < 5; ++i) { // BFS 탐색 전 방문 배열 초기화
+            fill(board[i], board[i] + 5, 0);
+            fill(visited[i], visited[i] + 5, false);
+        }
+        
+        for (int i = 0; i < 25; ++i) {
+            if (mask[i] == 0) {
+                int x = i / 5, y = i % 5;
+                if (seat[x][y] == 'S') ++S;
+                board[x][y] = 1;
+                v.push_back({x, y});
+            }
+        }
+
+        ans += (S >= 4 && bfs(v));
     } while (next_permutation(mask, mask + 25)); // 25C7 = 480700
 
     cout << ans;
+
     return 0;
 }
-
