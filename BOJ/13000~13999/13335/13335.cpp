fdd8737ce381d109229c8fcd4065b6fd1ad2209b
heejinn <hjlee6824@naver.com>
2024-04-19 15:15:55 +0900

    트럭

--- BOJ/13000~13999/13335/13335.cpp
+++ BOJ/13000~13999/13335/13335.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int n, w, L;
 
-queue<int> q;
+queue<int> truck;
 // 현재 다리 위에 트럭의 무게와 트럭이 움직인 거리를 저장함
 deque<pair<int, int>> bridge;
 
@@ -13,32 +13,32 @@ int main() {
 
     cin >> n >> w >> L;
 
-    for (int i = 0; i < n; ++i) {
-        int truck;
-        cin >> truck;
-        q.push(truck);
+    while (n--) {
+        int input;
+        cin >> input;
+        truck.push(input);
     }
 
     int ans = 0;
     int total_weight = 0;
     while (true) {
-        if (q.empty() && bridge.empty()) break;
+        if (truck.empty() && bridge.empty()) break;
 
         // 다리에 있는 트럭들을 1칸씩 옮긴다
         if (!bridge.empty())
             for (auto& e : bridge) ++e.second;
 
-        // 맨 앞 트럭이 다리를 완전히 지나가는 경우
+        // 맨 앞 트럭이 다리에서 나가는 경우
         if (!bridge.empty() && bridge.front().second > w) {
             total_weight -= bridge.front().first;
             bridge.pop_front();
         }
 
         // 다리에 올라갈 수 있는 경우
-        if (!q.empty() && bridge.size() < w && total_weight + q.front() <= L) {
-            bridge.push_back({q.front(), 1});
-            total_weight += q.front();
-            q.pop();
+        if (!truck.empty() && bridge.size() < w && total_weight + truck.front() <= L) {
+            bridge.push_back({truck.front(), 1});
+            total_weight += truck.front();
+            truck.pop();
         }
 
         // 시간을 1만큼 진행
