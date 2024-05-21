cfe5e30796284979930f5e43f9f8dbb99d4d862f
heejinn <hjlee6824@naver.com>
2024-05-22 00:55:44 +0900

    16진수

---
+++ Baekjoon/1000~1999/1550/1550.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int input;
+    cin >> hex >> input;
+    cout << input;
+    return 0;
+}
