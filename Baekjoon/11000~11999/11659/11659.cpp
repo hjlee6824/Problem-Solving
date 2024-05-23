8950b6758132f691fab98d7ec5da76bd5f2a3fb9
heejinn <hjlee6824@naver.com>
2024-05-23 11:34:20 +0900

    구간 합 구하기 4

--- Baekjoon/11000~11999/11659/11659.cpp
+++ Baekjoon/11000~11999/11659/11659.cpp
@@ -1,31 +1,35 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
+int n, m;
+int arr[100001];
+int d[100001];
+
+int main() {
+    ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int N, M;
-    cin >> N >> M;
 
-    vector<int> prefixSum(N + 1);
+    cin >> n >> m;
 
-    for (int i = 1; i <= N; ++i)
-    {
-        int input;
-        cin >> input;
-        prefixSum[i] = prefixSum[i - 1] + input;
+    for (int i = 1; i <= n; ++i) {
+        cin >> arr[i];
+        d[i] = d[i - 1] + arr[i];
     }
 
-    while (M--)
-    {
+    while (m--) {
         int i, j;
         cin >> i >> j;
-
-        cout << prefixSum[j] - prefixSum[i - 1] << '\n';
+        cout << d[j] - d[i - 1] << '\n';
     }
     
     return 0;
 }
+
+// 테이블 정의
+// d[i]: 1번째부터 i번째까지의 합
+
+// 점화식
+// d[i] = d[i - 1] + arr[i]
+
+// 초기값
+// d[0] = 0
