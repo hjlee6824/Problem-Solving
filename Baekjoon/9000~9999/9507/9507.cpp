36bd17f7738a14673188bbd4146de5c152bfc554
heejinn <hjlee6824@naver.com>
2024-06-12 02:18:26 +0900

    Generations of Tribbles

---
+++ Baekjoon/9000~9999/9507/9507.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int n;
+long long koong[68];
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    koong[0] = 1;
+    koong[1] = 1;
+    koong[2] = 2;
+    koong[3] = 4;
+
+    for (int i = 4; i <= 67; ++i)
+        koong[i] = koong[i - 1] + koong[i - 2] + koong[i - 3] + koong[i - 4];
+
+    cin >> t;
+
+    while (t--) {
+        cin >> n;
+        cout << koong[n] << '\n';
+    }
+
+    return 0;
+}
