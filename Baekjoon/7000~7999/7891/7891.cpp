9cc004898cb904a8b4ce2f91099925cfeffdd4b5
heejinn <hjlee6824@naver.com>
2024-06-18 01:22:22 +0900

    Can you add this?

---
+++ Baekjoon/7000~7999/7891/7891.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> t;
+
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        cout << x + y << '\n';
+    }
+
+    return 0;
+}
