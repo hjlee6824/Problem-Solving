badaa2c08c2334812ea8c07ce39451cae3c400e6
heejinn <hjlee6824@naver.com>
2024-06-27 03:38:16 +0900

    스위트콘 가격 구하기

---
+++ Baekjoon/30000~30999/30030/30030.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> b;
+    cout << b / 11 * 10;
+    
+    return 0;
+}
