c75c96576eb4f996b589e00a41d311e092411565
heejinn <hjlee6824@naver.com>
2024-06-28 02:13:00 +0900

    특별한 학교 이름

---
+++ Baekjoon/27000~27999/27889/27889.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string str;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> str;
+
+    if (str == "NLCS") cout << "North London Collegiate School";
+    else if (str == "BHA") cout << "Branksome Hall Asia";
+    else if (str == "KIS") cout << "Korea International School";
+    else cout << "St. Johnsbury Academy";
+
+    return 0;
+}
