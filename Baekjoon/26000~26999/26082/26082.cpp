9a660524b38a36ff6406546c335bad8d8b930ee2
heejinn <hjlee6824@naver.com>
2024-07-07 01:41:26 +0900

    WARBOY

---
+++ Baekjoon/26000~26999/26082/26082.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b, c;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> a >> b >> c;
+    cout << (b / a) * 3 * c;
+    return 0;
+}
