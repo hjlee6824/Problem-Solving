bb5f547ab3c1988c6ac2c6f5254ff28a6316d657
heejinn <hjlee6824@naver.com>
2024-07-13 01:12:39 +0900

    Cupcake Party

---
+++ Baekjoon/24000~24999/24568/24568.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int r, s;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> r >> s;
+    cout << r * 8 + s * 3 - 28;
+    return 0;
+}
