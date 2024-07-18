8e40c036d578750b54f148b36ddbb4a487fdab11
heejinn <hjlee6824@naver.com>
2024-07-18 20:56:12 +0900

    Copier

---
+++ Baekjoon/26000~26999/26574/26574.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+
+    while (n--) {
+        int input;
+        cin >> input;
+        cout << input <<  ' ' << input << '\n';
+    }
+
+    return 0;
+}
