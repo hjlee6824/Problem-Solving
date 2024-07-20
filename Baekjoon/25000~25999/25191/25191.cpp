5da2aa79ef75ac5cf7eb2869db7d6de651d843d9
heejinn <hjlee6824@naver.com>
2024-07-21 01:38:48 +0900

    치킨댄스를 추는 곰곰이를 본 임스

---
+++ Baekjoon/25000~25999/25191/25191.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, a, b;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> a >> b;
+    cout << min(n, a / 2 + b);
+    return 0;
+}
