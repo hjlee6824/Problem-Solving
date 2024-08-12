d1f4b99f996f2415d130f4f437d9bdbc9b2142d4
heejinn <hjlee6824@naver.com>
2024-08-12 20:18:46 +0900

    더하기 3

---
+++ Baekjoon/11000~11999/11023/11023.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int ans;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int input;
+    while (cin >> input) ans += input;
+    cout << ans;
+    return 0;
+}
